6d4bf0329754447c6a98bd042621a76a79991f72
MateuszB90 <116488337+MateuszB90@users.noreply.github.com>
2022-11-07 20:00:28 +0100

    Add files via upload
    Zadanie 6-11-2022 Mateusz Bugowski

---
+++ script.js
@@ -0,0 +1,24 @@
+
+
+const fullName  = () => {
+    const showFullName = document.getElementById("title");
+    showFullName.textContent = "Mateusz Bugowski"
+    
+}
+
+
+
+const Name = () => {
+    const showName = document.getElementById("title2");
+    showName.textContent = ("Mateusz")
+}
+
+const SecondName = () => {
+    const showSecondName = document.getElementById("title3");
+    showSecondName.textContent = ("Bugowski")
+}
+
+fullName();
+Name();
+SecondName();
+
